d7483211b6ac1fed264d2ef532d1f07c2a5077d2
Zackry Langford <zackrylangford@gmail.com>
2022-04-30 12:49:19 -0400

    cleaned again

--- zip_zip_racing.py
+++ zip_zip_racing.py
@@ -35,8 +35,7 @@ class ZipZipRacing:
         self.bullets = pygame.sprite.Group()
         self.asteroids = pygame.sprite.Group()
         self._create_belt()
-
-
+        
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -45,67 +44,6 @@ class ZipZipRacing:
             self._update_bullets()
             self._update_asteroids()
             self._update_screen()
-
-    def _create_belt(self):
-        """Create a belt of asteroids."""
-        # Make an asteroid.
-        asteroid = Asteroid(self)
-        asteroid.x = 2550
-        asteroid.rect.x = asteroid.x
-        asteroid.rect.y = randint (25,1200)
-        self.asteroids.add(asteroid)
-        self.asteroids.update()
-
-    def _update_asteroids(self):
-        """Update the positions of the asteroids in the belt."""
-        self.asteroids.update()
-        # Get rid of asteroids that go off the screen 
-        for asteroid in self.asteroids.copy():
-            if asteroid.rect.x <= -5:
-                self.asteroids.remove(asteroid)
-        if not self.asteroids:
-        # Create a new asteroid and send it
-            self._create_belt()
-
-        # Decrement amount of asteroids left in level
-        self.stats.asteroids_left -= 1
-
-        if pygame.sprite.spritecollideany(self.scooter, self.asteroids):
-            self._scooter_hit()
-
-    def _scooter_hit(self):
-        """Respond to the ship being hit by an alien."""
-
-        # Decrement scooters_left.
-        self.stats.scooters_left -= 1
-
-        # Get rid of any remaining aliens and bullets.
-        self.asteroids.empty()
-        self.bullets.empty()
-
-        # Create a new fleet and center the ship.
-        self._create_belt()
-        self.scooter.center_scooter()
-
-        # Pause
-        sleep(0.5)
-
-     
-    def _update_bullets(self):
-        """Update position of bullets and get rid of old bullets"""
-        # Update bullet positions.
-        self.bullets.update()
-
-        # Get rid of bullets that have disappeared. 
-        for bullet in self.bullets.copy():
-            if bullet.rect.x >= self.settings.screen_width:
-                self.bullets.remove(bullet)
-
-        # Check for bullets that have hit asteroids
-        # If so, get rid of the bullet and the asteroid.
-        collisions = pygame.sprite.groupcollide(
-            self.bullets, self.asteroids, True, True) 
-
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -146,6 +84,71 @@ class ZipZipRacing:
         if len(self.bullets) < self.settings.bullets_allowed:    
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets"""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared. 
+        for bullet in self.bullets.copy():
+            if bullet.rect.x >= self.settings.screen_width:
+                self.bullets.remove(bullet)
+
+        # Check for bullets that have hit asteroids
+        # If so, get rid of the bullet and the asteroid.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.asteroids, True, True) 
+
+    def _update_asteroids(self):
+        """Update the positions of the asteroids in the belt."""
+        self.asteroids.update()
+        # Get rid of asteroids that go off the screen 
+        for asteroid in self.asteroids.copy():
+            if asteroid.rect.x <= -5:
+                self.asteroids.remove(asteroid)
+        if not self.asteroids:
+        # Create a new asteroid and send it
+            self._create_belt()
+
+        # Decrement amount of asteroids left in level
+        self.stats.asteroids_left -= 1
+
+        if pygame.sprite.spritecollideany(self.scooter, self.asteroids):
+            self._scooter_hit()
+
+    def _create_belt(self):
+        """Create a belt of asteroids."""
+        # Make an asteroid.
+        asteroid = Asteroid(self)
+        asteroid.x = 2550
+        asteroid.rect.x = asteroid.x
+        asteroid.rect.y = randint (25,1200)
+        self.asteroids.add(asteroid)
+        self.asteroids.update()
+
+    def _scooter_hit(self):
+        """Respond to the ship being hit by an alien."""
+
+        # Decrement scooters_left.
+        self.stats.scooters_left -= 1
+
+        # Get rid of any remaining aliens and bullets.
+        self.asteroids.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_belt()
+        self.scooter.center_scooter()
+
+        # Pause
+        sleep(0.5)
+
+     
+
+
+
+ 
 
 
     def _update_screen(self):
