8e4ef0c8759a5e5caf3448eabc2a342b875e1150
Zackry Langford <zackrylangford@gmail.com>
2022-04-30 13:26:00 -0400

    not full screen start

--- zip_zip_racing.py
+++ zip_zip_racing.py
@@ -23,7 +23,7 @@ class ZipZipRacing:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((1200,800))
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Zip Zip Racing")
